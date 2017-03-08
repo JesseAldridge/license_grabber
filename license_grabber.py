fabe61e03f7cdc0ba8fca3397fdb538f489891e1
Jesse Aldridge <JesseAldridge@gmail.com>
2017-03-07 19:04:57 -0800

    check classifiers as fallback

--- license_grabber.py
+++ license_grabber.py
@@ -2,21 +2,37 @@ import json, re
 
 import requests
 
-with open('packages.txt') as f:
-  text = f.read()
-lines = text.splitlines()
-name_version = [re.split('==|<=|>=|<|>', line) for line in lines]
 
-for t in name_version:
-  try:
-    name, version = t
-  except ValueError:
-    print 'could not process:', t
-  json_str = requests.get('https://pypi.python.org/pypi/{}/json'.format(name)).content
+def main():
+  with open('packages.txt') as f:
+    text = f.read()
+  lines = text.splitlines()
+  name_version = [re.split('==|<=|>=|<|>', line) for line in lines]
+
+  for t in name_version:
+    try:
+      package_name, version = t
+    except ValueError:
+      print 'could not process:', t
+    get(package_name)
+
+def get(package_name, debug=False):
+  json_str = requests.get('https://pypi.python.org/pypi/{}/json'.format(package_name)).content
   package_dict = json.loads(json_str)
+  if debug:
+    print json.dumps(package_dict, indent=2)
   license_str = package_dict['info']['license']
+  if license_str == 'UNKNOWN':
+    for classifier in package_dict['info']['classifiers']:
+      if classifier.startswith('License'):
+        license_str = classifier
+        break
   for safe_license in ('MIT', 'BSD', 'Apache', 'PSFL'):
     if re.search(r'\b{}\b'.format(safe_license), license_str):
       break
   else:
-    print name, license_str
+    print package_name, license_str
+
+if __name__ == '__main__':
+  main()
+  # get('zipstream', debug=True)
